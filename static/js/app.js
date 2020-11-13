47f8caa079edc58a01cb79fa281c0b4bb00b0717
Katherine Schuman <katieschuman@katherinesair2.attlocal.net>
2020-11-12 21:26:59 -0500

    building updateplotly function

--- static/js/app.js
+++ static/js/app.js
@@ -3,59 +3,50 @@
 // var sampleValues = []
 // var subject_ids = []
 
-// Retrieve data from samples.json file using D3
-d3.json("samples.json").then((data) => {
-    // console.log(data);
+// initialize the page with a default plot 
+var oneHundreds = [];
+var twoHundreds = [];
+var threeHundreds = [];
+var fourHundreds = [];
+var fiveHundreds = [];
+var sixHundreds = [];
+var eightHundreds = [];
 
-    // Separate data out into useable pieces for rendering into charts
-    // Array of Test Subject IDs
-    var testSubjectNames = data.names;
-    // console.log(testSubjectNames);
+function init() {
 
-    // Array of objects -- each object containing one test subject's demographic info
-    var demographicInfo = data.metadata
-    // console.log(demographicInfo);
+    var groups = ["100's", "200's", "300's", "400's", "500's", "600's", "800's"]
 
-    // Array of objects -- each object containing one test subject's samples
-    var samplesInfo = data.samples;
-    console.log(samplesInfo);
+    // Retrieve data from samples.json file using D3
+    d3.json("samples.json").then((data) => {
+        // console.log(data);
 
-    // Pull out sample_values (arrays of each sample value for each user)
-    var sampleValues = samplesInfo.map(sample => sample.sample_values);
-    console.log(sampleValues);
+        // Separate data out into useable pieces for rendering into charts
+        // Array of Test Subject IDs
+        var testSubjects = data.names;
+        console.log(testSubjects);
 
-    //Pull out otu_ids (arrays of otu ids for each user)
-    var otuIDs = samplesInfo.map(sample => sample.otu_ids);
-    console.log(otuIDs);
+        // Array of objects -- each object containing one test subject's demographic info
+        var demographicInfo = data.metadata
+        // console.log(demographicInfo);
 
+        // Array of objects -- each object containing one test subject's samples
+        var samplesInfo = data.samples;
+        console.log(samplesInfo);
 
-    ////// Building drop-down ///////
-    var dropDown = d3.select("#selDataset")
+        // Pull out sample_values (arrays of each sample value for each user)
+        var sampleValues = samplesInfo.map(sample => sample.sample_values);
+        console.log(sampleValues);
 
-    testSubjectNames.forEach((subID) => {
-        dropDown
-            .append("testSubjectNames")
-            .text(subID)
-            .property("value");
-    });
-
-
-
-    // Call updatePlotly() when a change takes place to the DOM
-    d3.selectAll("#selDataset").on("change", updatePlotly);
-
-    // This function is called when a dropdown menu item is selected
-    function updatePlotly() {
-        // Use D3 to select the dropdown menu
-        var dropdownMenu = d3.select("#selDataset");
-        // Assign the value of the dropdown menu option to a variable
-        var dataset = dropdownMenu.property("value");
+        //Pull out otu_ids (arrays of otu ids for each user)
+        var otuIDs = samplesInfo.map(sample => sample.otu_ids);
+        console.log(otuIDs);
 
 
 
 
 
-        // ////////Dummy Data for Testing////////////
+
+        // ////////Dummy Data for Setting Up Charts////////////
         var otu_ids = [1, 2, 3, 4, 5];
         var sample_values = [1, 2, 4, 8, 16];
         var otu_labels = ["one", "two", "three", "four", "five"]
@@ -105,3 +96,38 @@ d3.json("samples.json").then((data) => {
         // Plot the chart to a div tag with id "bubble"
         Plotly.newPlot("bubble", data2, layout2);
     });
+
+
+
+
+    // Call updatePlotly() when a change takes place to the DOM
+    d3.selectAll("#selDataset").on("change", updatePlotly);
+
+    // This function is called when a dropdown menu item is selected
+    function updatePlotly() {
+
+        ////// Building drop-down ///////
+        // Use D3 to select the dropdown menu
+        var dropDown = d3.select("#selDataset");
+
+        // Assign the value of the dropdown menu option to a variable
+        var dataset = testSubjects.forEach((subjID) => {
+            dropDown
+                .append("testSubjects")
+                .text(subjID)
+                .property("value");
+        });
+
+
+
+
+        // restyle the graph based on the dropdown selection 
+        Plotly.restyle("bar", "x", [x]);
+        Plotly.restyle("bar", "y", [y]);
+        Plotly.restyle("bubble", "x", [x]);
+        Plotly.restyle("bubble", "y", [y]);
+
+
+    }
+    init();
+};
